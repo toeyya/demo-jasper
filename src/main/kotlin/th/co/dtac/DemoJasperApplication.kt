9a8d1ea0e34aced24882b1326e25bf41979777f1
araya tansinchai <toey@odd-e.co.th>
2016-11-06 22:10:12 +0700

    Add thai language

--- src/main/kotlin/th/co/dtac/DemoJasperApplication.kt
+++ src/main/kotlin/th/co/dtac/DemoJasperApplication.kt
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import th.co.dtac.model.Country
+import java.nio.charset.Charset
 
 
 @SpringBootApplication
@@ -17,17 +18,29 @@ open class DemoJasperApplication{
     @Bean
     open fun init() = CommandLineRunner {
 
+        var string = "abc\u0E40\u0E15\u0E49\u0E22"
+        val utf8 = string.toByteArray(charset("UTF-8"))
+        string = kotlin.text.String(utf8, Charset.forName("UTF-8"))
+        println(string)
+
+
         val jrxmlfile : String= "/home/araya/Workspace/demo-jasper/src/main/kotlin/jasperreport/jasper_report_template.jrxml"
+        val data = Country()
+        data.name = "\u0E2D\u0E32\u0E23\u0E0D\u0E32"
+        data.country = "Thailand"
+        val data1 = Country()
+        data1.name = "\u0E40\u0E15\u0E49\u0E22"
+        data1.country = "English"
+        val data2 = Country()
+        data2.name = "Araya"
+        data2.country = "France"
+
+        val dataList : MutableList<Country> = mutableListOf()
+        dataList.add(data)
+        dataList.add(data1)
+        dataList.add(data2)
+
         try{
-            val data = Country()
-            data.name = "Araya"
-            data.country = "Thailand"
-            val data1 = Country()
-            data1.name = "John"
-            data1.country = "English"
-            val dataList : MutableList<Country> = mutableListOf()
-            dataList.add(data)
-            dataList.add(data1)
             val beanColDataSource = JRBeanCollectionDataSource(dataList)
             val jasperReport = JasperCompileManager.compileReportToFile(jrxmlfile)
             val parameters : MutableMap<String,Any> = mutableMapOf()
---
+++ src/main/kotlin/th/co/dtac/model/Country.kt
@@ -0,0 +1,6 @@
+package th.co.dtac.model
+
+class Country{
+    var name: String = ""
+    var country: String = ""
+}
